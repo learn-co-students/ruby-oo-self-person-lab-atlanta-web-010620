f600ce7ad4027ae7696872a67a1a921814faf2e1
Dino <dinokunovac@gmail.com>
2020-01-08 16:23:28 -0500

    instant

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+    def initialize(name)
+        @name = name
+    end
+
+    def bank_account
+        money = 25
+    end
+
+    def happiness
+        level = 8
+    end
+end
