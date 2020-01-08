8969e14dd315f1ce937cf429855b96f64c5abf3e
Alvin Coulson <aaacoulson@gmail.com>
2020-01-08 18:29:07 -0500

    Finish

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,85 @@
 # your code goes here
+require 'pry'
 class Person
-    attr_reader :name
-    attr_accessor :bank_account, :happiness
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
     def initialize(name)
         @name = name
+        @happiness = 8
+        @bank_account = 25
+        @hygiene = 8
     end
 
-    def bank_account
-        money = 25
+    def happiness=(num)
+        if num >= 0 && num <= 10
+            @happiness = num
+        elsif num < 0
+            @happiness = 0
+        else
+            @happiness = 10
+        end
     end
 
-    def happiness
-        level = 8
+    def hygiene=(smell)
+        if smell >= 0 && smell <= 10
+            @hygiene = smell
+        elsif smell < 0
+            @hygiene = 0
+        else
+            @hygiene = 10
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        #binding.pry
+        self.bank_account += salary
+        return "all about the benjamins"
+    end
+        
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    
+    def work_out
+        self.happiness += 2 
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
     end
 end
